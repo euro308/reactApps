582ed10392a117e2dff9782750bbee9022f1d75f
Adam Bendík <euro307@email.cz>
2024-05-13 19:20:35 +0200

    Dokončení Stránky

--- first-tailwind-project/src/app/page.tsx
+++ first-tailwind-project/src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
             {/* Name section */}
             <div>
                 <div className={"flex justify-between items-center w-screen p-10 pt-20"}>
-                    <h1 className={"text-9xl font-extrabold"}>NOLAN</h1>
+                    <h1 className={"text-[220px] font-extrabold"}>NOLAN</h1>
                     <img src={"https://framerusercontent.com/images/7pK0QrLlKy390Qs8FadnwPTqts.png?scale-down-to=1024"}
                          alt={"img1"} className={"w-2/6"}/>
                 </div>
@@ -25,7 +25,7 @@ export default function Home() {
                 <div className={"flex justify-between items-center w-screen p-10 pt-20"}>
                     <img src={"https://framerusercontent.com/images/16Rx6Rm8Wa2Dq7bs97DMY2Nwc.jpg?scale-down-to=512"}
                          alt={"img1"} className={"w-2/6"}/>
-                    <h1 className={"text-9xl font-extrabold"}>MITCHELL</h1>
+                    <h1 className={"text-[220px] font-extrabold"}>MITCHELL</h1>
                 </div>
             </div>
 
@@ -96,14 +96,27 @@ export default function Home() {
             </div>
 
             {/* Info about me */}
-            Odkaz: https://nolanmitchell.framer.website/
+            <div className={"flex justify-start items-center p-10 pt-0 pb-0 mt-40"}>
+                <img src={"https://framerusercontent.com/images/11emWfyEkMUl97UDec8wLF4.jpeg?scale-down-to=1024"}
+                     alt={"nolan-mitchell"} className={"w-5/12 object-cover"}/>
+
+                <div className={"flex justify-center items-start w-2/6 flex-col ml-24"}>
+                    <span className={"text-7xl font-bold"}>
+                        <span className={"font-mono italic font-light"}>from</span> LONDON ALL THE <span
+                        className={"font-mono italic font-light"}>way</span>
+                        <br/> TO <br/> BEAUTIFUL MELBOURNE
+                    </span>
+                    <span className={"pt-20 text-xs"}>With years of experience under my belt, I'm passionate about helping people like you find their perfect slice of Melbourne real estate heaven. Whether you're searching for your dream home, an investment property, or looking to sell, I'm here to guide you through every step of the process with expertise and enthusiasm.</span>
+                    <span  className={"pt-20 text-xs font-bold hover:text-gray-500"}>(Find out more about me)</span>
+                </div>
+            </div>
 
 
             {/* Socials */}
-            <div className={"flex justify-between items-center p-10 pt-0 pb-0"}>
-                <h1 className={"text-7xl font-bold w-1/6"}>NOLAN MITCHELL</h1>
+            <div className={"flex justify-between items-center p-10 mt-24 pb-0"}>
+                <h1 className={"text-9xl font-bold w-1/6"}>NOLAN MITCHELL</h1>
 
-                <div className={"flex flex-col font-bold gap-2"}>
+                <div className={"flex flex-col font-bold gap-10 text-xl"}>
                     <span>LinkedIn</span>
                     <span>Instagram</span>
                     <span>X (Twitter)</span>
@@ -111,7 +124,7 @@ export default function Home() {
 
                 <div className={"flex justify-center items-center flex-col"}>
                     <span className={"font-bold text-sm"}>Melbourne, AUS</span>
-                    <span className={"text-sm"}>9:59 AM</span>
+                    <span className={"text-sm"}>7:14 AM</span>
                 </div>
             </div>
         </main>
