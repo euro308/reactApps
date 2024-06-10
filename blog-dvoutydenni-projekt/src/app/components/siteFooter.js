e559e252369bc61cbb368a69450d88cec4bf285e
Adam Bendík <euro307@email.cz>
2024-06-10 13:08:03 +0200

    Project Finished

--- blog-dvoutydenni-projekt/src/app/components/siteFooter.js
+++ blog-dvoutydenni-projekt/src/app/components/siteFooter.js
@@ -1,6 +1,6 @@
 export function SiteFooter() {
     return (
-        <header className={"min-w-full flex items-center justify-center fixed bottom-0"}>
+        <header className={"min-w-full flex items-center justify-center"}>
             <span>Euro's Blog, made by Adam Bendík</span>
         </header>
     )
